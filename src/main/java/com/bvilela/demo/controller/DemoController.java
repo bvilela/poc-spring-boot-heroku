68ff49d73582c5c1609b1b0ca6f03b22f41c95bd
Bruno Vilela <brunovilela2008@hotmail.com>
2022-07-22 22:17:18 -0300

    add actuator

--- src/main/java/com/bvilela/demo/controller/DemoController.java
+++ src/main/java/com/bvilela/demo/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.bvilela.demo;
+package com.bvilela.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,14 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	@GetMapping("/method1")
+	@GetMapping("/method-1")
 	public String method1() {
 		return "Greetings from Spring Boot! - 1";
 	}
 	
-	@GetMapping("/method2")
+	@GetMapping("/method-2")
 	public String method2() {
 		return "Greetings from Spring Boot! - 2";
+	}
+	
+	@GetMapping("/version")
+	public String version() {
+		return "0.0.1";
 	}
 	
 }
